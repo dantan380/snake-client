4503beb3dfad10f7712b510193bd83394826b48b
Daniel Tan <danieltan380@gmail.com>
2023-09-27 15:16:41 -0700

    Added conn.on to let us know when the client is successfully connected to the server. Identified our snake with a name

--- client.js
+++ client.js
@@ -14,6 +14,14 @@ const connect = function () {
     console.log("Server says:", data);
   });
 
+  conn.on("connect", () => {
+    console.log("Successfully connected to game server.");
+  });
+
+  conn.on("connect", () => {
+    conn.write("Name: DAN");
+  });
+
   return conn;
 };
 
