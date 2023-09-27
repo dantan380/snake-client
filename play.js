3bb258a0fc76f3bbcdc6dc18dab9fef70f653653
Daniel Tan <danieltan380@gmail.com>
2023-09-27 15:43:14 -0700

    Added setUpInput to read keyboard inputs. Implemented handleUserInput to ctrl + c is recognized to terminate the game

--- play.js
+++ play.js
@@ -2,3 +2,20 @@ const connect = require('./client');
 
 console.log("Connecting ...");
 connect();
+
+const handleUserInput = function (key) {
+    if (key === '\u0003'){
+        process.exit();
+    }
+  }
+
+const setupInput = function () {
+    const stdin = process.stdin;
+    stdin.on("data", handleUserInput);
+    stdin.setRawMode(true);
+    stdin.setEncoding("utf8");
+    stdin.resume();
+    return stdin;
+  };
+
+setupInput();
