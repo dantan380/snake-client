fc275bab4c00404fc32efa4cd15752a85ba0d0ea
Daniel Tan <danieltan380@gmail.com>
2023-09-27 15:52:06 -0700

    Imported setupInput with require.

--- play.js
+++ play.js
@@ -1,21 +1,8 @@
-const connect = require('./client');
+const {connect} = require('./client');
+const {setupInput} = require('./input');
 
 console.log("Connecting ...");
 connect();
 
-const handleUserInput = function (key) {
-    if (key === '\u0003'){
-        process.exit();
-    }
-  }
-
-const setupInput = function () {
-    const stdin = process.stdin;
-    stdin.on("data", handleUserInput);
-    stdin.setRawMode(true);
-    stdin.setEncoding("utf8");
-    stdin.resume();
-    return stdin;
-  };
 
 setupInput();
