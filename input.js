a92d2100414321101b6f9e6be39edd73d5e47e1d
Daniel Tan <danieltan380@gmail.com>
2023-09-27 17:33:56 -0700

    Renamed setupInput parameter to be more clear. Added connection.write for snake movement commands

--- input.js
+++ input.js
@@ -1,10 +1,9 @@
-const handleUserInput = function (key) {
-    if (key === '\u0003'){
-        process.exit();
-    }
-  }
+let connection;
 
-const setupInput = function () {
+
+
+const setupInput = function (anActualConnectionThatHasBeenCreated) {
+    connection = anActualConnectionThatHasBeenCreated;
     const stdin = process.stdin;
     stdin.on("data", handleUserInput);
     stdin.setRawMode(true);
@@ -14,3 +13,21 @@ const setupInput = function () {
   };
 
   module.exports = {setupInput};
+
+  const handleUserInput = function (key) {
+    if (key === '\u0003'){
+        process.exit();
+    };
+    if (key === 'w'){
+        connection.write("Move: up");
+    }
+    if (key === 'a'){
+        connection.write("Move: left");
+    }
+    if (key === 's'){
+        connection.write("Move: down");
+    }
+    if (key === 'd'){
+        connection.write("Move: right");
+    }
+  }
